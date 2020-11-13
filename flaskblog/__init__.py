3b2d4e199065cccaad9d8b5294454000b0baf089
srujan2000 <59117944+srujan2000@users.noreply.github.com>
2020-11-13 18:51:14 +0530

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '287f64321cef7a86260f283853b99a90'
+app.config['SECRET_KEY'] = os.environ['secret_key']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
